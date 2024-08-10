db5ca5a693fee62e1c0a2a0d5190e3947f574b02
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-10 12:29:02 -0300

    criando rotas produto

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Home from './views/Home';
+import Produto from './views/Produto';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/produto" element={<Produto />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ front/src/views/Produto/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Produto = () => {
+    return (
+        <>
+        <h1>Produto</h1>
+        </>
+    )
+}
+
+export default Produto;
