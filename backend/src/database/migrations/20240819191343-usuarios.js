7192c172025caec963e39a4557b08d397149e952
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-19 16:21:24 -0300

    Update 20240819191343-usuarios.js

--- backend/src/database/migrations/20240819191343-usuarios.js
+++ backend/src/database/migrations/20240819191343-usuarios.js
@@ -3,10 +3,42 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      nome: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      senha: {  
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      admin: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      },
+    });
 
   },
 
   async down(queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('Users');
 
   }
 };
