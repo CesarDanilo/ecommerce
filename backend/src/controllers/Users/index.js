cfcd9f7544c547c83f2cd0055c6dd52b8d6b7f24
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-20 17:28:31 -0300

    Update index.js

---
+++ backend/src/controllers/Users/index.js
@@ -0,0 +1,11 @@
+const inserirUsers = require('./inserir');
+const editarUsers = require('./editar');
+const excluirUsers = require('./excluir');
+const listarUsers = require('./listar');
+
+module.exports = {
+    inserirUsers,
+    editarUsers,
+    excluirUsers,
+    listarUsers
+}
