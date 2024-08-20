dddcf7ddc7aed005647615af6b57e2f5be9f4697
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-20 17:27:27 -0300

    Update excluir.js

--- backend/src/controllers/Users/excluir.js
+++ backend/src/controllers/Users/excluir.js
@@ -1,1 +1,33 @@
+const { Users } = require('../../database/models');
 
+const excluirUsers = async (req, resp, next) => {
+
+    const { id } = req.params;
+    try {
+
+        let result = await Users.findByPk(id);
+
+        if (!result)
+            return resp.status(404).json({ msg: `Users ID ${id} não encontrado` });
+
+        try {
+            result = await Users.destroy({ where: { id } });
+        }
+        catch (error) {
+            const msg = 'Erro ao tentar Excluir!';
+            const erro = error?.message;
+            return resp.status(400).json({ msg, erro });
+        }
+
+        return resp.status(200).json({ msg: 'Excluído com Sucesso' });
+    }
+    catch (error) {
+
+        const msg = 'Users. Erro ao tentar Excluir (generic).';
+        const erro = error?.message;
+        return resp.status(400).json({ msg, erro });
+    }
+}
+
+
+module.exports = excluirUsers;
