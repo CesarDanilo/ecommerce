8488c2fc0d27dd68251d19af4d118eea7b5e779d
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-20 17:25:11 -0300

    Update editar.js

--- backend/src/controllers/Users/editar.js
+++ backend/src/controllers/Users/editar.js
@@ -1,1 +1,34 @@
+const { Users } = require('../../database/models');
 
+const editarUsers = async (req, resp, next) => {
+
+    const { id } = req.params;
+    try {
+
+        const dados = req.body;
+        let result = await Users.findByPk(id);
+
+        if (!result)
+            return resp.status(404).json({ msg: `Users ID ${id} não encontrado` });
+
+        try {
+            await Users.update(dados, { where: { id } });
+        }
+        catch (error) {
+            const msg = 'Erro ao tentar Gravar!';
+            const erro = error?.message;
+            return resp.status(400).json({ msg, erro });
+        }
+
+        result = await Users.findByPk(id);
+
+        return resp.status(200).json({ msg: 'Gravado com Sucesso', data: result.dataValues });
+    }
+    catch (error) {
+        const msg = 'Users. Erro ao tentar Editar (generic).';
+        const erro = error?.message;
+        return resp.status(400).json({ msg, erro });
+    }
+}
+
+module.exports = editarUsers;
