fdbe3a715b6e9b8ee11481e71d6454ac94203ac1
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-28 10:33:35 -0300

    Update index.jsx

--- front/src/Components/Produto/ProdutosRelacionados/index.jsx
+++ front/src/Components/Produto/ProdutosRelacionados/index.jsx
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Grid, Box } from '@mui/material';
-import Link from '@mui/material/Link';
+import { Link } from 'react-router-dom';
+import axios from "axios";
 
 const ProdutosRelacionados = () => {
-    const imgs = [
-        { title: 'MousePad Speed', url: 'https://ae-pic-a1.aliexpress-media.com/kf/S2a0513a05487456ea2ba32dc6113cfefS.jpg_640x640.jpg_.webp', description: 'Mousepad 40x45', preco: '180,00' },
-        { title: 'MousePad Speed', url: 'https://ae-pic-a1.aliexpress-media.com/kf/Saa92b6d67ba9470b9fa9fa4b0a6f778a6.jpg_640x640.jpg_.webp', description: 'Mousepad 40x45', preco: '180,00' },
-        { title: 'MousePad Speed', url: 'https://ae-pic-a1.aliexpress-media.com/kf/Sfee60a210944451ab6a6c3913ccd9069x.jpg_640x640.jpg_.webp', description: 'Mousepad 40x45', preco: '180,00' },
-    ];
+    const [data, setData] = useState([]); // Inicializa como array vazio
+    const baseURL = "http://localhost:3001/produto/";
+
+    const BuscarDadosProduto = async () => {
+        try {
+            const res = await axios.get(baseURL);
+            const { data } = res.data; // Verifique a estrutura da resposta
+            setData(data);
+            console.log(data);
+        } catch (erro) {
+            console.log("não foi possível buscar os dados: ", erro);
+        }
+    }
+
+    useEffect(() => {
+        BuscarDadosProduto();
+    }, [])
+
     return (
         <Box
             sx={{
                 maxWidth: 1000,
-                margin: '0 auto',  // Centraliza o conteúdo
-                padding: '0 16px', // Adiciona um pequeno preenchimento nas laterais
-                width: '100%',     // Garante que o Box ocupe 100% da largura disponível
+                margin: '0 auto',
+                padding: '0 16px',
+                width: '100%',
             }}
         >
             <Typography
@@ -35,40 +49,46 @@ const ProdutosRelacionados = () => {
                 Produtos Relacionados
             </Typography>
             <Grid container spacing={3}>
-                {imgs.map((i, index) => (
-                    <Grid item xs={12} sm={6} md={4} key={index}>
-                        <Link href="/produto" underline="none" color="black" fontWeight="bold">
-                            <Card sx={{ maxWidth: '100%', margin: 'auto' }}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        height="190"
-                                        image={i.url}
-                                        alt="mouse pad"
-                                    />
-                                    <CardContent
-                                        sx={{
-                                            textAlign: 'left',  
-                                            padding: '15px',
-                                            display: 'flex',
-                                            flexDirection: 'column'
-                                        }}
-                                    >
-                                        <Typography gutterBottom component="div" fontWeight="bold" mt={'2px'}>
-                                            {i.title}
-                                        </Typography>
-                                        <Typography variant="body3" color="text.secondary" mt={'1px'}>
-                                            {i.description}
-                                        </Typography>
-                                        <Typography variant="body3" color="text.primary" fontWeight="bold" mt={'2px'}>
-                                            {`R$ ${i.preco}`}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Link>
-                    </Grid>
-                ))}
+                {data.length > 0 ? (
+                    data.map((i, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index}>
+                            <Link to={`/produto/${i.id}`} underline="none" color="black" fontWeight="bold">
+                                <Card sx={{ maxWidth: '100%', margin: 'auto' }}>
+                                    <CardActionArea>
+                                        <CardMedia
+                                            component="img"
+                                            height="190"
+                                            image={`http://localhost:3001/uploads/${i.imagem}`}
+                                            alt="mouse pad"
+                                        />
+                                        <CardContent
+                                            sx={{
+                                                textAlign: 'left',
+                                                padding: '15px',
+                                                display: 'flex',
+                                                flexDirection: 'column'
+                                            }}
+                                        >
+                                            <Typography gutterBottom component="div" fontWeight="bold" mt={'2px'}>
+                                                {i.nome}
+                                            </Typography>
+                                            <Typography variant="body2" color="text.secondary" mt={'1px'}>
+                                                {i.descricao}
+                                            </Typography>
+                                            <Typography variant="body2" color="text.primary" fontWeight="bold" mt={'2px'}>
+                                                {`R$ ${i.preco}`}
+                                            </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </Link>
+                        </Grid>
+                    ))
+                ) : (
+                    <Typography variant="body1" color="text.secondary">
+                        Nenhum produto relacionado encontrado.
+                    </Typography>
+                )}
             </Grid>
         </Box>
     )
