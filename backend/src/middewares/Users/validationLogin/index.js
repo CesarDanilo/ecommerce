37c78867384ccec3f991fcb85423bcc0007ac922
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-31 11:38:50 -0300

    Update index.js
    CRIANDO A CRIPTOGRAFIA DA SENHA DO USUARIO

--- backend/src/middewares/Users/validationLogin/index.js
+++ backend/src/middewares/Users/validationLogin/index.js
@@ -1,21 +1,38 @@
+const { Users } = require("../../../database/models/");
+const bcrypt = require("bcrypt");
+
 const validationLogin = async (req, res, next) => {
     try {
         const { nome, email, senha } = req.body;
 
         if (!nome) {
-            return res.status(422).json({ msg: "O nome é obrigatorio!" });
+            return res.status(422).json({ msg: "O nome é obrigatório!" });
         }
         if (!email) {
-            return res.status(422).json({ msg: "O email é obrigatorio!" });
+            return res.status(422).json({ msg: "O e-mail é obrigatório!" });
         }
         if (!senha) {
-            return res.status(422).json({ msg: "O senha é obrigatorio!" });
+            return res.status(422).json({ msg: "A senha é obrigatória!" });
         }
 
-    } catch (error) {
-        console.log("OPS! ocorreu um erro de middleware!", error)
-    }
+        // check if users exists
+        const usersExists = await Users.findOne({ where: { email: email } });
+        if (usersExists) {
+            return res.status(422).json({ msg: "Já existe um usuário com esse e-mail, por favor utilize outro!" });
+        }
 
-}
+        //create password
+        const salt = await bcrypt.genSalt(12);
+        const passwordHash = await bcrypt.hash(senha, salt);
+
+        req.body.senha = passwordHash;
+
+        next();
+
+    } catch (error) {
+        console.log("OPS! ocorreu um erro de middleware!", error);
+        return res.status(500).json({ msg: "Ocorreu um erro interno no servidor." });
+    }
+};
 
 module.exports = validationLogin;
