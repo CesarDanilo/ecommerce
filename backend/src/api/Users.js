e753de167f926eae0dda0889cacf7d4d1d31707a
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-08-31 11:51:47 -0300

    create middleware for login

--- backend/src/api/Users.js
+++ backend/src/api/Users.js
@@ -2,11 +2,13 @@ const express = require('express');
 const rotas = express.Router();
 
 const UsersControllers = require('../controllers/Users');
+const validationCreateLogin = require('../middewares/Users/validationCreateLogin')
 const validationLogin = require('../middewares/Users/validationLogin')
 
 rotas.get('/', UsersControllers.listarUsers);
 
-rotas.post('/', validationLogin, UsersControllers.inserirUsers);
+rotas.post('/', validationCreateLogin, UsersControllers.inserirUsers);
+rotas.post('/auth/login', validationLogin);
 
 rotas.put('/:id', UsersControllers.editarUsers);
 
