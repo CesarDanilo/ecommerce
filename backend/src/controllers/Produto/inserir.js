8188c90aea163d24900f048be5d9539cda5f39cd
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-09-05 00:31:23 -0300

    refatorando codigos

--- backend/src/controllers/Produto/inserir.js
+++ backend/src/controllers/Produto/inserir.js
@@ -4,24 +4,17 @@ const path = require('path');
 const inserirProduto = async (req, res, next) => {
     try {
         const { nome, preco, descricao, estoque } = req.body;
-        const imagem_name = req.file.path.replace(/\\/g, "/"); // Corrigido para armazenar o caminho da imagem corretamente
+        const imagem_name = req.file.path.replace(/\\/g, "/");
         const imagem = path.basename(imagem_name)
-        console.log("nome da img",imagem)
+        console.log("nome da img", imagem)
 
         // Salve as informações no banco de dados, incluindo o caminho da imagem
-        const produto = {
-            nome,
-            preco,
-            descricao,
-            estoque,
-            imagem,
-        };
+        const produto = { nome, preco, descricao, estoque, imagem };
 
         // Supondo que você esteja usando Sequelize ou qualquer outro ORM
-        const savedProduct = await Produto.create(produto); // ajuste de acordo com o seu ORM
-
+        const savedProduct = await Produto.create(produto);
         res.json(savedProduct);
-        
+
         console.log("Sucesso ao salvar produto:");
     } catch (error) {
         console.error("Erro ao salvar produto:", error);
