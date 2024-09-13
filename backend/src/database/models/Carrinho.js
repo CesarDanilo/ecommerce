bf9bf091e092414738179dbbfc1af56a7453a175
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-09-13 17:37:12 -0400

    Update Carrinho.js

---
+++ backend/src/database/models/Carrinho.js
@@ -0,0 +1,57 @@
+'use strict';
+const { Model, DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    class Carrinho extends Model {
+        // Definir associações, se necessário
+        static associate(models) {
+            // Exemplo de associação, se aplicável:
+            // Carrinho.belongsTo(models.User, { foreignKey: 'usuario_id' });
+            // Carrinho.belongsTo(models.Produto, { foreignKey: 'produto_id' });
+        }
+    };
+
+    Carrinho.init({
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        usuario_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Users',  // Certifique-se de que o nome do modelo está correto
+                key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+        },
+        produto_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'Produto',  // Certifique-se de que o nome do modelo está correto
+                key: 'id',
+            },
+            onUpdate: 'CASCADE',
+            onDelete: 'SET NULL'
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW
+        }
+    }, {
+        sequelize,
+        modelName: 'Carrinho',
+        tableName: 'Carrinho',
+        timestamps: true  // Se 'createdAt' e 'updatedAt' devem ser geridos automaticamente
+    });
+
+    return Carrinho;
+};
