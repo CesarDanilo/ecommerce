c5810d59a9110956ce0d048edaf6f14abfe47f8b
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-09-14 08:45:07 -0400

    CREATED API (CARRINHO)

---
+++ backend/src/api/Carrinho.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const rotas = express.Router();
+
+const CarrinhoControllers = require('../controllers/Carrinho');
+
+rotas.get('/', CarrinhoControllers.listarCarrinho);
+
+rotas.post('/', CarrinhoControllers.inserirCarrinho);
+
+rotas.put('/:id', CarrinhoControllers.editarCarrinho);
+
+rotas.delete('/:id', CarrinhoControllers.excluirCarrinho);
+
+module.exports = rotas;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,7 @@ const pedidoProdutoRoutes = require('../api/PedidoProduto');
 const pedidoRoutes = require('../api/Pedido');
 const produtoRoutes = require('../api/Produto');
 const usersRoutes = require('../api/Users');
+const carrinhoRoutes = require('../api/Carrinho');
 
 // Definindo as rotas
 rotas.use('/favoritos', favoritosRoutes);
@@ -13,5 +14,6 @@ rotas.use('/pedidoProduto', pedidoProdutoRoutes);
 rotas.use('/pedido', pedidoRoutes);
 rotas.use('/produto', produtoRoutes);
 rotas.use('/users', usersRoutes);
+rotas.use('/carrinho', carrinhoRoutes);
 
 module.exports = rotas;
