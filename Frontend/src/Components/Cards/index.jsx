e484e710d069be81c8f7b9c2341b275c571bf6c6
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-09-25 13:48:08 -0400

    Update index.jsx

--- Frontend/src/Components/Cards/index.jsx
+++ Frontend/src/Components/Cards/index.jsx
@@ -66,6 +66,19 @@ const Cards = ({ titulo }) => {
                                 <div className="p-4 text-left flex flex-col space-y-1">
                                     <h3 className="font-semibold text-base text-gray-900">{i.nome}</h3>
                                     <p className="text-gray-600 text-sm">{i.descricao}</p>
+                                    <div className="flex space-x-1 mt-1">
+                                        {[...Array(5)].map((_, starIndex) => (
+                                            <svg
+                                                key={starIndex}
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className={`h-5 w-5 ${starIndex < i.avaliacao ? 'text-yellow-400' : 'text-gray-300'}`}
+                                                fill="currentColor"
+                                                viewBox="0 0 24 24"
+                                            >
+                                                <path d="M12 .587l3.668 7.431 8.197 1.188-5.916 5.788 1.396 8.173L12 18.896l-7.345 3.858 1.396-8.173-5.916-5.788 8.197-1.188z" />
+                                            </svg>
+                                        ))}
+                                    </div>
                                     <p className="text-black font-bold text-lg">{`R$ ${i.preco}`}</p>
                                 </div>
                             </div>
@@ -73,6 +86,7 @@ const Cards = ({ titulo }) => {
                     </div>
                 ))}
             </div>
+
 
 
         </Box>
