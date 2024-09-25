1f0ee4b9c9995c73415e7936bad3756392138b81
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-09-25 14:21:48 -0400

    Update index.jsx

--- Frontend/src/Components/NavBar/index.jsx
+++ Frontend/src/Components/NavBar/index.jsx
@@ -26,64 +26,6 @@ const NavBar = ({ qntProd }) => {
     }));
 
     return (
-        // <Grid container spacing={2} sx={{ padding: '0 16px', margin: '10px 0px' }}>
-        //     <Grid item xs={12} sm={4} md={2}>
-        //         <Link href="/" underline="none" color="black" fontWeight="bold">
-        //             <Box
-        //                 sx={{
-        //                     display: 'flex',
-        //                     justifyContent: 'center',
-        //                     alignItems: 'center',
-        //                     padding: "10px",
-        //                 }}
-        //             >
-        //                 <img src={Logo} alt="Logo" style={{ maxWidth: '100%', height: '80px' }} />
-        //             </Box>
-        //         </Link>
-        //     </Grid>
-        //     <Grid item xs={12} sm={8} md={9.5}>
-        //         <Box
-        //             sx={{
-        //                 display: 'flex',
-        //                 flexWrap: 'wrap',
-        //                 justifyContent: 'flex-end',
-        //                 alignItems: 'center',
-        //                 padding: "10px 0",
-        //                 typography: 'body1',
-        //             }}
-        //         >
-        //             <Stack direction="row" spacing={2} alignItems="center">
-        //                 <Button>
-        //                     <Link href="/" underline="none" color="black" fontWeight="bold">
-        //                         {'HOME'}
-        //                     </Link>
-        //                 </Button>
-        //                 <Button>
-        //                     <Link href="http://localhost:5173/" underline="none" color="black" fontWeight="bold">
-        //                         {'DASHBOARD'}
-        //                     </Link>
-        //                 </Button>
-        //                 <Button>
-        //                     <Link href="#" underline="none" color="black" fontWeight="bold">
-        //                         {'CONTATO'}
-        //                     </Link>
-        //                 </Button>
-        //                 <Link href="/carrinho" underline="none" color="black" fontWeight="bold">
-        //                     <IconButton aria-label="cart">
-        //                         <StyledBadge badgeContent={qntProd} color="secondary">
-        //                             <ShoppingCartIcon />
-        //                         </StyledBadge>
-        //                     </IconButton>
-        //                 </Link>
-        // <Button>
-        //     <Link href="#" underline="none" color="black" fontWeight="bold">
-        //         <Avatar sx={{ bgcolor: deepPurple[500] }}>C</Avatar>
-        //     </Link>
-        // </Button>
-        //             </Stack>
-        //         </Box>
-        //     </Grid>
-        // </Grid>
         <div className="flex flex-col md:flex-row justify-between items-center p-4 mx-4 my-2">
             <Link href="/" className="flex items-center mb-4 md:mb-0">
                 <img src={Logo} alt="Logo" className="max-w-full h-20" />
