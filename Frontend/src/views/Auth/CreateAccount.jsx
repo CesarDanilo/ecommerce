730925d2df4de5f35455b90273202c010163b18d
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-01 14:30:42 -0400

    Update CreateAccount.jsx

--- Frontend/src/views/Auth/CreateAccount.jsx
+++ Frontend/src/views/Auth/CreateAccount.jsx
@@ -16,7 +16,7 @@ export function CreateAccount() {
     return (
         <section className="m-8 flex">
             <div className="w-2/5 h-full hidden lg:block">
-                <img src="/img/pattern.png" className="h-full w-full object-cover rounded-3xl" />
+                <img src={`http://localhost:3001/uploads/img/person.jpg`} className="h-full w-full object-cover rounded-3xl" />
             </div>
             <div className="w-full lg:w-3/5 flex flex-col items-center justify-center">
                 <div className="text-center">
