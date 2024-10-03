6a461219f931033303504ba3094709cb647e2c29
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-03 14:52:19 -0400

    UPDATE MIGRATE CARRINHO

--- backend/src/database/migrations/20241003011946-carrinho.js
+++ backend/src/database/migrations/20241003011946-carrinho.js
@@ -2,21 +2,31 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Carrinho', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      usuario_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Usuario',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      }
+    }
+    );
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface, Sequelize) {
+
+    await queryInterface.dropTable('Carrinho');
+
   }
 };
