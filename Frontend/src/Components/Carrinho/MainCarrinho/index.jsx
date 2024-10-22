9e5825df9a28eac713d16c31994ff153656a0c9b
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-22 00:35:23 -0300

    Update index.jsx

--- Frontend/src/Components/Carrinho/MainCarrinho/index.jsx
+++ Frontend/src/Components/Carrinho/MainCarrinho/index.jsx
@@ -1,139 +1,128 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Box, Typography, Stack, Button } from '@mui/material';
+import { Grid, Box, Typography, Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import RemoveIcon from '@mui/icons-material/Remove';
+import axios from "axios";
 
 const MainCarrinho = () => {
-    const [subtotal, setSubtotal] = useState(0);
-    const [frete, setFrete] = useState(50);
-    const [totalEstimado, setTotalEstimado] = useState();
+    const baseUrl = "http://localhost:3001/carrinho";
+    const [produtos, setProdutos] = useState([]);  // Inicializando como array
 
-    const imgs = [
-        { title: 'MousePad Speed', url: 'https://ae-pic-a1.aliexpress-media.com/kf/S2a0513a05487456ea2ba32dc6113cfefS.jpg_640x640.jpg_.webp', description: 'Mousepad 40x45', preco: '180,00' },
-        { title: 'MousePad Speed', url: 'https://ae-pic-a1.aliexpress-media.com/kf/Saa92b6d67ba9470b9fa9fa4b0a6f778a6.jpg_640x640.jpg_.webp', description: 'Mousepad 40x45', preco: '180,00' },
-        { title: 'MousePad Speed', url: 'https://ae-pic-a1.aliexpress-media.com/kf/Saa92b6d67ba9470b9fa9fa4b0a6f778a6.jpg_640x640.jpg_.webp', description: 'Mousepad 40x45', preco: '180,00' },
-    ];
+    const buscarIdDeUsuario = () => {
+        const usuarioString = localStorage.getItem("user");
+        if (usuarioString) {
+            const usuario = JSON.parse(usuarioString); // Converte a string em objeto
+            return usuario.id; // Retorna o campo 'id'
+        }
+        return null; // Caso não encontre o usuário
+    };
 
-    function BuscarDados() {
-        let tempSubtotal = 0;
-        imgs.forEach((i) => {
-            tempSubtotal += parseFloat(i.preco.replace(',', '.'));
-        });
-        setSubtotal(tempSubtotal);
-        setTotalEstimado(subtotal + frete)
-    }
+    const buscarDadosDoCarrinho = async () => {
+        try {
+            const idUsuario = buscarIdDeUsuario();
+            if (!idUsuario) {
+                console.log("Usuário não encontrado no localStorage");
+                return;
+            }
+
+            const response = await axios.get(`${baseUrl}?usuario_id=1`);
+
+            // Verifica se response.data é um array, caso contrário, define como vazio
+            if (Array.isArray(response.data[0])) {
+                setProdutos(response.data); // Se for um array, atualiza o estado
+            } else {
+                setProdutos([]); // Caso contrário, define como array vazio
+            }
+
+            console.log("Produtos do Carrinho:", response.data);
+
+        } catch (error) {
+            console.log("Aconteceu alguma coisa de errado: ", error);
+        }
+    };
 
     useEffect(() => {
-        BuscarDados();
-    }, [subtotal, setSubtotal])
+        buscarDadosDoCarrinho();
+    }, []); // Chamando a função quando o componente for montado
 
     return (
-        <Box sx={{
-            maxWidth: 1000,
-            margin: '0 auto',
-            padding: '16px',
-        }}>
+        <Box
+            sx={{
+                maxWidth: 1000,
+                margin: '0 auto',
+                padding: '16px',
+            }}
+        >
             <Grid container spacing={4}>
                 <Grid item xs={12} sm={6}>
-                    {imgs.map((i, index) => (
-                        <Grid container spacing={2} key={index}>
-                            <Grid item xs={12} sm={5}>
-                                <Box sx={{ padding: 1 }}>
-                                    <img src={i.url} alt={i.title} style={{
-                                        width: '100%',
-                                        borderRadius: 10
-                                    }} />
-                                </Box>
-                            </Grid>
-
-                            <Grid item xs={12} sm={7}>
-                                <Box sx={{
-                                    display: 'flex',
-                                    flexDirection: 'column',
-                                    justifyContent: "flex-start",
-                                    alignItems: 'flex-start',
-                                    padding: 1
-                                }}>
-                                    <Typography variant="h6" sx={{ fontWeight: "bold" }}>
-                                        {i.title}
-                                    </Typography>
-
-                                    <Typography variant="body1" sx={{ color: 'darkgrey', marginTop: 1 }}>
-                                        {i.description}
-                                    </Typography>
-
-                                    <Typography variant="body1" sx={{ marginTop: 1 }}>
-                                        R$ {i.preco}
-                                    </Typography>
-
-                                    <Box sx={{
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                        marginTop: 2
-                                    }}>
-                                        <Button size="small">
-                                            <RemoveIcon />
-                                        </Button>
-                                        <Typography variant="body1" sx={{ marginLeft: 2 }}>
-                                            {imgs.length}
-                                        </Typography>
-                                        <Button size="small">
-                                            <AddIcon />
-                                        </Button>
+                    {produtos.length > 0 ? ( // Verifica se há produtos no array
+                        produtos.map((produto, index) => (
+                            <Grid container spacing={2} key={index}>
+                                <Grid item xs={12} sm={5}>
+                                    <Box sx={{ padding: 1 }}>
+                                        <img
+                                            src={produto.url} // Suponho que seja a URL da imagem do produto
+                                            alt={produto.title} // Título do produto
+                                            style={{
+                                                width: '100%',
+                                                borderRadius: 10,
+                                            }}
+                                        />
                                     </Box>
-                                </Box>
+                                </Grid>
+
+                                <Grid item xs={12} sm={7}>
+                                    <Box
+                                        sx={{
+                                            display: 'flex',
+                                            flexDirection: 'column',
+                                            justifyContent: 'flex-start',
+                                            alignItems: 'flex-start',
+                                            padding: 1,
+                                        }}
+                                    >
+                                        <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                            {produto.title}
+                                        </Typography>
+
+                                        <Typography variant="body1" sx={{ color: 'darkgrey', marginTop: 1 }}>
+                                            {produto.description}
+                                        </Typography>
+
+                                        <Typography variant="body1" sx={{ marginTop: 1 }}>
+                                            R$ {produto.preco}
+                                        </Typography>
+
+                                        <Box
+                                            sx={{
+                                                display: 'flex',
+                                                alignItems: 'center',
+                                                marginTop: 2,
+                                            }}
+                                        >
+                                            <Button size="small">
+                                                <RemoveIcon />
+                                            </Button>
+
+                                            <Typography variant="body1" sx={{ marginLeft: 2 }}>
+                                                {produto.quantidade} {/* Quantidade do produto no carrinho */}
+                                            </Typography>
+
+                                            <Button size="small">
+                                                <AddIcon />
+                                            </Button>
+                                        </Box>
+                                    </Box>
+                                </Grid>
                             </Grid>
-                        </Grid>
-                    ))}
-                </Grid>
-
-                <Grid item xs={12} sm={6}>
-                    <Box sx={{ padding: 2 }}>
-                        <Typography variant="h4" sx={{ fontWeight: "bold" }}>
-                            Total
-                        </Typography>
-                        <Box sx={{
-                            display: 'flex',
-                            justifyContent: 'space-between',
-                            padding: 2,
-                            marginTop: 2
-                        }}>
-                            <Box>
-                                <Typography variant="body1" sx={{ fontWeight: "bold", marginTop: 2, textAlign: 'start' }}>
-                                    Subtotal
-                                </Typography>
-
-                                <Typography variant="body1" sx={{ fontWeight: "bold", marginTop: 1, textAlign: 'start' }}>
-                                    Frete
-                                </Typography>
-
-                                <Typography variant="body1" sx={{ fontWeight: "bold", marginTop: 1, textAlign: 'start' }}>
-                                    Total Estimado
-                                </Typography>
-                            </Box>
-
-                            <Box>
-                                <Typography variant="body1" sx={{ fontWeight: "bold", marginTop: 2, textAlign: 'right' }}>
-                                    R$ {subtotal}
-                                </Typography>
-
-                                <Typography variant="body1" sx={{ fontWeight: "bold", marginTop: 1, textAlign: 'right' }}>
-                                    R$ {frete}
-                                </Typography>
-
-                                <Typography variant="body1" sx={{ fontWeight: "bold", marginTop: 1, textAlign: 'right' }}>
-                                    R$ {totalEstimado}
-                                </Typography>
-                            </Box>
-                        </Box>
-                        <Stack spacing={2} direction="row">
-                            <Button variant="contained" sx={{ backgroundColor: 'black', width: '100%', marginTop: '25px' }}>Finalizar</Button>
-                        </Stack>
-                    </Box>
+                        ))
+                    ) : (
+                        <Typography variant="body1">Carrinho vazio.</Typography> // Exibe mensagem se não houver produtos
+                    )}
                 </Grid>
             </Grid>
         </Box>
-    )
-}
+    );
+};
 
 export default MainCarrinho;
