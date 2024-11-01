1dead6bc4d91573664f90f0872cf679177ef1be3
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-10-31 21:31:26 -0300

    CREATED QRCODE CHECKOUT

--- Frontend/src/Components/Checkout/index.jsx
+++ Frontend/src/Components/Checkout/index.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const CheckoutPagamento = () => {
     return (
-        <div>
-            
+        <div className="flex justify-center items-center p-4 mt-4">
+            <div className="w-72">
+                <img
+                    src="http://localhost:3001/uploads/img/frame.png"
+                    alt="Descrição da imagem"
+                    className="w-full h-auto"
+                />
+            </div>
         </div>
-    )
+    );
 }
 
 export default CheckoutPagamento;
--- Frontend/src/views/Checkout/index.jsx
+++ Frontend/src/views/Checkout/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import NavBar from "Components/NavBar";
 import Footer from "Components/Footer";
 import ProdutosRelacionados from "Components/Produto/ProdutosRelacionados";
-
+import CheckoutPagamento from "Components/Checkout";
 const Checkout = () => {
     return (
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <NavBar />
+            <CheckoutPagamento />
             <ProdutosRelacionados />
             <Footer />
         </div>
